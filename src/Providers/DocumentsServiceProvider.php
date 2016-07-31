8df768f04bdf88848d97b90d0979df2940549d9e
Egemen Berker Kızılcan <egemenkizilcan@gmail.com>
2016-07-31 17:48:48 +0300

    Updated dependencies

--- src/Providers/DocumentsServiceProvider.php
+++ src/Providers/DocumentsServiceProvider.php
@@ -9,7 +9,7 @@ use Nuclear\Documents\Repositories\DocumentsRepository;
 
 class DocumentsServiceProvider extends ServiceProvider {
 
-    const version = '0.9.2';
+    const version = '0.9.3';
 
     /**
      * Indicates if loading of the provider is deferred.
