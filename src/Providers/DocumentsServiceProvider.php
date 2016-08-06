60595684ecf2c252a8bd9b8013279a12d7166cd0
Egemen Berker Kızılcan <egemenkizilcan@gmail.com>
2016-08-06 12:45:35 +0300

    Migration fix

--- src/Providers/DocumentsServiceProvider.php
+++ src/Providers/DocumentsServiceProvider.php
@@ -9,7 +9,7 @@ use Nuclear\Documents\Repositories\DocumentsRepository;
 
 class DocumentsServiceProvider extends ServiceProvider {
 
-    const version = '0.9.5';
+    const version = '0.9.6';
 
     /**
      * Indicates if loading of the provider is deferred.
